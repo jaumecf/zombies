74a564639bc2c1fd78b5a319748c37080a4458fc
jaumecf <caf@paucasesnovescifp.cat>
2023-02-02 20:30:23 +0100

    FPS controlled and Zombie animated

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensibility = 100f;
+
+    // Referència al transform del jugador FPS
+    public Transform playerBody;
+
+    private float xRotation;
+
+    void Start()
+    {
+        // Limitar a que no sortiguem dels límits de la pantalla
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensibility * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensibility * Time.deltaTime;
+
+        // diferència entre xRotation i mouseY
+        xRotation -= mouseY;
+        // filtram per a que no passi del màxim i del mínim
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // Rotació eix horitzontal
+        playerBody.Rotate(Vector3.up * mouseX);
+        // Rotació eix vertial
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Variable de Character Controller per al moviment horitzontal i vertical
+    public CharacterController controller;
+    public float speed = 12f;
+
+    // Definició de variables per a la gravetat
+    private Vector3 velocity;
+    public float gravity = -9.81f;
+
+    // GroundCheck
+    public bool isGrounded;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f; //Umbral de distància enterra
+    public LayerMask groundMask;
+
+    // Jump
+    public float jumpHeight = 2f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // Mirar si estic tocant el terra
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2;
+        }
+
+        // Valors entre -1 i 1
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        // Gravetat
+        // Formula de velocitat = acceleració * temps^2
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+        //print(velocity.y);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+    }
+}
